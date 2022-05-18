654260e6d2759ef826a90b5cce098f41766b6b61
syo0e <kimdlzp@gmail.com>
2022-05-18 22:52:22 +0900

    JSX.md

--- react-practice/blog/src/App.js
+++ react-practice/blog/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let post = "강남 우동 맛집";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+        <div>{post}</div>
+      </div>
     </div>
   );
 }
