23d56063e0a983e895a56cc1604d654061e4f6e9
syo0e <kimdlzp@gmail.com>
2022-05-18 23:13:49 +0900

    state

--- react-practice/blog/src/App.js
+++ react-practice/blog/src/App.js
@@ -1,12 +1,17 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  let post = "강남 우동 맛집";
+  let [글제목, b] = useState("남자 코트 추천");
+  let posts = "강남 우동 맛집";
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
-        <div>{post}</div>
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
       </div>
     </div>
   );
