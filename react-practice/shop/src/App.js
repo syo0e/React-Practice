b76026f4acc293fc6d94214471f7831865cc72b8
syo0e <kimdlzp@gmail.com>
2022-05-24 16:53:15 +0900

    import export

--- react-practice/shop/src/App.js
+++ react-practice/shop/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js'
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
@@ -20,8 +23,8 @@ function App() {
         <div className='row'>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
