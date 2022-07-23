0e6d49ebae67a294bb99a44ac3ffa719e52a4895
syo0e <kimdlzp@gmail.com>
2022-07-23 14:28:22 +0900

    Card

--- react-practice/shop/src/App.js
+++ react-practice/shop/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import { useState } from 'react';
-import data from './data.js'
-
+import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { useState } from "react";
+import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
   let [shoes] = useState(data);
   return (
@@ -17,27 +17,30 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
+      <div className="main-bg"></div>
 
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-4'>
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className='col-md-4'>
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className='col-md-4'>
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} />;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
