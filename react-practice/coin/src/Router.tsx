1cef82e57b7ae6c8a34bd1852b2ceaf3c386065b
syo0e <kimdlzp@gmail.com>
2022-08-10 18:33:33 +0900

    styled

--- react-practice/coin/src/Router.tsx
+++ react-practice/coin/src/Router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Coin from "./routes/Coin";
-import Coins from "./routes/Coins";
+import Coin from "../routes/Coin";
+import Coins from "../routes/Coins";
 
 function Router() {
   return (
--- react-practice/coin/src/index.tsx
+++ react-practice/coin/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { theme } from "./theme";
+import { theme } from "../theme";
 
 ReactDOM.render(
   <React.StrictMode>
